d0a671df2b83db78540f3f489d683243232af20e
helencho <helencho@ac.c4q.nyc>
2017-10-30 15:53:01 -0400

    added var currentPosition, heightCounter, and top; reassign to these variables after every if statement

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
 // Stacks intro h1 elements to one another 
 jQuery(document).ready(function () {
     window.onscroll = function () {
-        var positionOne = jQuery("#scroll-screen-one").offset();
-        var positionTwo = jQuery("#scroll-screen-two").offset();
-        var positionThree = jQuery("#scroll-screen-three").offset();
-        var positionFour = jQuery("#scroll-screen-four").offset();
-        var positionFive = jQuery("#scroll-screen-five").offset();
-        var positionSix = jQuery("#scroll-screen-six").offset();
-        var positionSeven = jQuery("#scroll-screen-seven").offset();
+        var currentPosition = jQuery("#scroll-screen-one").offset();
         var heightCounter = 0;
+        var top = "0";
 
-        if (window.pageYOffset >= positionOne.top) {
-            jQuery("#sticky-one").css({ position: 'fixed', top: '0' });
+        if (window.pageYOffset >= currentPosition.top) {
+            jQuery("#sticky-one").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-one").height();
+            currentPosition = jQuery("#scroll-screen-two").offset();
+            top =+ (parseInt(top) + 80).toString();
         }
-        if (window.pageYOffset >= positionTwo.top - heightCounter) {
-            jQuery("#sticky-two").css({ position: 'fixed', top: '80px' });
+        if (window.pageYOffset >= currentPosition.top - heightCounter) {
+            // console.log('first', 'top: ' + top)
+            jQuery("#sticky-two").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-two").height();
+            currentPosition = jQuery("#scroll-screen-three").offset();
+            top =+ (parseInt(top) + 80).toString();
         }
-        if (window.pageYOffset >= positionThree.top - heightCounter) {
-            jQuery("#sticky-three").css({ position: 'fixed', top: '160px' });
+        if (window.pageYOffset >= currentPosition.top - heightCounter) {
+            jQuery("#sticky-three").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-three").height();
+            currentPosition = jQuery("#scroll-screen-four").offset();
+            top =+ (parseInt(top) + 80).toString();
         }
-        if (window.pageYOffset >= positionFour.top - heightCounter) {
-            jQuery("#sticky-four").css({ position: 'fixed', top: '240px' });
+        if (window.pageYOffset >= currentPosition.top - heightCounter) {
+            jQuery("#sticky-four").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-four").height();
+            currentPosition = jQuery("#scroll-screen-five").offset();
+            top =+ (parseInt(top) + 80).toString();
         } 
-        if (window.pageYOffset >= positionFive.top - heightCounter) {
-            jQuery("#sticky-five").css({ position: 'fixed', top: '320px' });
+        if (window.pageYOffset >= currentPosition.top - heightCounter) {
+            jQuery("#sticky-five").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-five").height();
+            currentPosition = jQuery("#scroll-screen-six").offset();
+            top =+ (parseInt(top) + 80).toString();
         } 
-        if (window.pageYOffset >= positionSix.top - heightCounter) {
-            jQuery("#sticky-six").css({ position: 'fixed', top: '400px' });
+        if (window.pageYOffset >= currentPosition.top - heightCounter) {
+            jQuery("#sticky-six").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-six").height();
+            currentPosition = jQuery("#scroll-screen-seven").offset();
+            top =+ (parseInt(top) + 80).toString();
         } 
-        if (window.pageYOffset >= positionSeven.top - heightCounter) {
-            jQuery("#sticky-seven").css({ position: 'fixed', top: '480px' });
+        if (window.pageYOffset >= currentPosition.top - heightCounter) {
+            jQuery("#sticky-seven").css({ position: 'fixed', top: top });
             heightCounter += jQuery("#sticky-seven").height();
+            currentPosition = jQuery("#scroll-screen-eight").offset();
+            top =+ (parseInt(top) + 80).toString();
         }
 
         else {
